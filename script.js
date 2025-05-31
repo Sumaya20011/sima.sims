6dc230f01400fd6f5e0e3d7cf9897007d184584a
Sumaya20011 <sumakam89@gmail.com>
2025-05-31 16:11:32 +0200

    add cat sima

---
+++ script.js
@@ -0,0 +1,16 @@
+// Анимация появления карточек при скролле
+const cards = document.querySelectorAll('.animate-on-scroll');
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    }
+  });
+}, {
+  threshold: 0.2
+});
+
+cards.forEach(card => {
+  observer.observe(card);
+});
